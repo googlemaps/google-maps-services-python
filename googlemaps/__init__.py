b44dd9773ff8d0ad37a67ef8f5adcb1ebcd4c4e7
Brett Morgan <brettmorgan@google.com>
2014-09-18 11:25:33 +1000

    Eliding TimeZone

--- googlemaps/__init__.py
+++ googlemaps/__init__.py
@@ -2,7 +2,6 @@ from googlemaps.common import Context
 from googlemaps.directions import directions
 from googlemaps.geocoding import geocode
 from googlemaps.geocoding import reverse_geocode
-from googlemaps.timezone import timezone
 
 # Allow sphinx to pick up these symbols for the documentation.
-__all__ = ['Context', 'geocode', 'reverse_geocode', 'directions', 'timezone']
+__all__ = ['Context', 'geocode', 'reverse_geocode', 'directions']
