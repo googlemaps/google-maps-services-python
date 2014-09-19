283f400ccc589b97d9817d65cf669c53783bc8e6
Luke Mahe <lukem@google.com>
2014-09-19 16:22:35 +1000

    Updating tests

--- test/test_elevation.py
+++ test/test_elevation.py
@@ -1,4 +1,4 @@
-"""Tests for the geocoding module."""
+"""Tests for the elevation module."""
 
 import unittest
 import datetime
@@ -12,25 +12,15 @@ class ElevationTest(unittest.TestCase):
             key="AIzaSyDyZdCabN8GKh786tdj16gq80xalbbfqDM",
             timeout=5)
 
-    _EPSILON = 0.000001
-
-    def _expected_elevation(self, expected_elevation, elevation):
-      elevation_delta = abs(elevation - expected_elevation)
-      self.assertTrue(elevation_delta < self._EPSILON)
-    
-    def _expected_resolution(self, expected_resolution, resolution):
-      resolution_delta = abs(resolution - expected_resolution)
-      self.assertTrue(resolution_delta < self._EPSILON)
-
     def test_elevation_single(self):
         results = googlemaps.elevation(self.c, (40.714728, -73.998672))
-        self._expected_elevation(8.883694, results[0]['elevation'])
-        self._expected_resolution(76.351615, results[0]['resolution'])
+        self.assertAlmostEqual(8.8836946, results[0]['elevation'])
+        self.assertAlmostEqual(76.3516159, results[0]['resolution'])
 
     def test_elevation_single_list(self):
         results = googlemaps.elevation(self.c, [(40.714728, -73.998672)])
-        self._expected_elevation(8.883694, results[0]['elevation'])
-        self._expected_resolution(76.351615, results[0]['resolution'])
+        self.assertAlmostEqual(8.8836946, results[0]['elevation'])
+        self.assertAlmostEqual(76.3516159, results[0]['resolution'])
 
     def test_elevation_multiple(self):
         locations = [(40.714728, -73.998672), (-34.397, 150.644)]
@@ -38,14 +28,15 @@ class ElevationTest(unittest.TestCase):
 
         self.assertTrue(2, len(results))
 
-        self._expected_elevation(8.883694, results[0]['elevation'])
-        self._expected_resolution(76.351615, results[0]['resolution'])
-        self._expected_elevation(392.511871, results[1]['elevation'])
-        self._expected_resolution(152.703231, results[1]['resolution'])
+        self.assertAlmostEqual(8.8836946, results[0]['elevation'], 4)
+        self.assertAlmostEqual(76.3516159, results[0]['resolution'])
+        self.assertAlmostEqual(392.5118713, results[1]['elevation'])
+        self.assertAlmostEqual(152.7032318, results[1]['resolution'])
 
     def test_elevation_along_path_single(self):
         with self.assertRaises(Exception):
-          results = googlemaps.elevation_along_path(self.c, [(40.714728, -73.998672)], 5)
+          results = googlemaps.elevation_along_path(self.c,
+                    [(40.714728, -73.998672)], 5)
 
     def test_elevation_along_path(self):
         path = [(40.714728, -73.998672), (-34.397, 150.644)]
@@ -54,8 +45,8 @@ class ElevationTest(unittest.TestCase):
 
         self.assertTrue(5, len(results))
 
-        self._expected_elevation(8.883694, results[0]['elevation'])
-        self._expected_resolution(76.351615, results[0]['resolution'])
-        self._expected_elevation(392.511871, results[4]['elevation'])
-        self._expected_resolution(152.703231, results[4]['resolution'])
+        self.assertAlmostEqual(8.8836946, results[0]['elevation'])
+        self.assertAlmostEqual(76.3516159, results[0]['resolution'])
+        self.assertAlmostEqual(392.5118713, results[4]['elevation'])
+        self.assertAlmostEqual(152.7032318, results[4]['resolution'])
 
