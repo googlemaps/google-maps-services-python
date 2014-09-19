fd43c03481aa8512c5e2c0d304d8c94554d0d729
Chris Broadfoot <cbro@google.com>
2014-09-19 12:32:29 +1000

    Merge test_useragent and test_common

--- test/test_common.py
+++ test/test_common.py
@@ -2,7 +2,7 @@
 
 import googlemaps
 import unittest
-
+import responses
 
 class CommonTest(unittest.TestCase):
 
@@ -15,3 +15,18 @@ class CommonTest(unittest.TestCase):
         with self.assertRaises(Exception):
             ctx = googlemaps.Context(key="Invalid key.")
             googlemaps.directions(ctx, "Sydney", "Melbourne")
+
+    @responses.activate
+    def test_ua_sent(self):
+        responses.add(responses.GET,
+                      "https://maps.googleapis.com/maps/api/geocode/json",
+                      body='{"status":"OK","results":[]}',
+                      status=200,
+                      content_type='application/json')
+
+        ctx = googlemaps.Context(key="AIzaasdf")
+        googlemaps.geocode(ctx, "Sesame St.")
+
+        self.assertEquals(1, len(responses.calls))
+        user_agent = responses.calls[0].request.headers["User-Agent"]
+        self.assertTrue(user_agent.startswith("GoogleGeoApiClientPython"))
