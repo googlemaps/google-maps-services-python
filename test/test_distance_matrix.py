796e98af2038266026c0fec265cef7afb1721d49
Brett Morgan <brettmorgan@google.com>
2014-09-29 17:19:08 +1000

    Mocking out server calls from unit tests

--- test/test_distance_matrix.py
+++ test/test_distance_matrix.py
@@ -2,15 +2,22 @@
 
 import unittest
 import googlemaps
-
+import responses
 
 class DistanceMatrixTest(unittest.TestCase):
 
     def setUp(self):
-        self.ctx = googlemaps.Context(
-            "AIzaSyAZ0_yiPw2Zp2huKxug49ZYi-pytL6NZ-c")
+        self.key = 'AIzaasdf'
+        self.ctx = googlemaps.Context(self.key)
 
+    @responses.activate
     def test_basic_params(self):
+        responses.add(responses.GET, 
+                      'https://maps.googleapis.com/maps/api/distancematrix/json',
+                      body='{"status":"OK","rows":[]}',
+                      status=200,
+                      content_type='application/json')
+
         origins = ["Perth, Australia", "Sydney, Australia",
                    "Melbourne, Australia", "Adelaide, Australia",
                    "Brisbane, Australia", "Darwin, Australia",
@@ -23,20 +30,46 @@ class DistanceMatrixTest(unittest.TestCase):
 
         matrix = googlemaps.distance_matrix(self.ctx, origins, destinations)
 
-        self.assertEqual(len(matrix["rows"]), 8)
-        self.assertEqual(len(matrix["rows"][0]["elements"]), 5)
+        self.assertEquals(1, len(responses.calls))
+        self.assertEquals('https://maps.googleapis.com/maps/api/distancematrix/json?'
+                          'key=%s&origins=Perth%%2C+Australia%%7CSydney%%2C+'
+                          'Australia%%7CMelbourne%%2C+Australia%%7CAdelaide%%2C+'
+                          'Australia%%7CBrisbane%%2C+Australia%%7CDarwin%%2C+'
+                          'Australia%%7CHobart%%2C+Australia%%7CCanberra%%2C+Australia&'
+                          'destinations=Uluru%%2C+Australia%%7CKakadu%%2C+Australia%%7C'
+                          'Blue+Mountains%%2C+Australia%%7CBungle+Bungles%%2C+Australia'
+                          '%%7CThe+Pinnacles%%2C+Australia' % self.key,
+                          responses.calls[0].request.url)
 
+    @responses.activate
     def test_mixed_params(self):
+        responses.add(responses.GET, 
+                      'https://maps.googleapis.com/maps/api/distancematrix/json',
+                      body='{"status":"OK","rows":[]}',
+                      status=200,
+                      content_type='application/json')
+        
         origins = ["Bobcaygeon ON", [41.43206, -81.38992]]
         destinations = [(43.012486, -83.6964149),
                         {"lat": 42.8863855, "lng": -78.8781627}]
 
         matrix = googlemaps.distance_matrix(self.ctx, origins, destinations)
 
-        self.assertEqual(len(matrix["rows"]), 2)
-        self.assertEqual(len(matrix["rows"][0]["elements"]), 2)
+        self.assertEquals(1, len(responses.calls))
+        self.assertEquals('https://maps.googleapis.com/maps/api/distancematrix/json?'
+                          'key=%s&origins=Bobcaygeon+ON%%7C41.432060%%2C-81.389920&'
+                          'destinations=43.012486%%2C-83.696415%%7C42.886386%%2C'
+                          '-78.878163' % self.key,
+                          responses.calls[0].request.url)
 
+    @responses.activate
     def test_all_params(self):
+        responses.add(responses.GET, 
+                      'https://maps.googleapis.com/maps/api/distancematrix/json',
+                      body='{"status":"OK","rows":[]}',
+                      status=200,
+                      content_type='application/json')
+
         origins = ["Perth, Australia", "Sydney, Australia",
                    "Melbourne, Australia", "Adelaide, Australia",
                    "Brisbane, Australia", "Darwin, Australia",
@@ -53,12 +86,27 @@ class DistanceMatrixTest(unittest.TestCase):
                                             avoid="tolls",
                                             units="imperial")
 
-        self.assertEqual(len(matrix["rows"]), 8)
-        self.assertEqual(len(matrix["rows"][0]["elements"]), 5)
-        self.assertTrue(matrix["rows"][0]["elements"][0]["distance"]["text"]
-                        .endswith("mi"))
+        self.assertEquals(1, len(responses.calls))
+        self.assertEquals('https://maps.googleapis.com/maps/api/distancematrix/json?'
+                          'origins=Perth%%2C+Australia%%7CSydney%%2C+Australia%%7C'
+                          'Melbourne%%2C+Australia%%7CAdelaide%%2C+Australia%%7C'
+                          'Brisbane%%2C+Australia%%7CDarwin%%2C+Australia%%7CHobart%%2C+'
+                          'Australia%%7CCanberra%%2C+Australia&language=en-AU&'
+                          'avoid=tolls&mode=driving&key=%s&units=imperial&'
+                          'destinations=Uluru%%2C+Australia%%7CKakadu%%2C+Australia%%7C'
+                          'Blue+Mountains%%2C+Australia%%7CBungle+Bungles%%2C+Australia'
+                          '%%7CThe+Pinnacles%%2C+Australia' % self.key,
+                          responses.calls[0].request.url)
 
+
+    @responses.activate
     def test_lang_param(self):
+        responses.add(responses.GET, 
+                      'https://maps.googleapis.com/maps/api/distancematrix/json',
+                      body='{"status":"OK","rows":[]}',
+                      status=200,
+                      content_type='application/json')
+
         origins = ["Vancouver BC", "Seattle"]
         destinations = ["San Francisco", "Victoria BC"]
 
@@ -66,9 +114,10 @@ class DistanceMatrixTest(unittest.TestCase):
                                             language="fr-FR",
                                             mode="bicycling")
 
-        # Ensure San Francisco is in les E'tats-Unis
-        self.assertTrue(matrix["destination_addresses"][0]
-                        .endswith(u'\xc9tats-Unis'))
-        # Ensure we get results in heures
-        self.assertTrue(matrix["rows"][0]["elements"][0]["duration"]["text"]
-                        .endswith(u'heures'))
+        self.assertEquals(1, len(responses.calls))
+        self.assertEquals('https://maps.googleapis.com/maps/api/distancematrix/json?'
+                          'key=%s&language=fr-FR&mode=bicycling&'
+                          'origins=Vancouver+BC%%7CSeattle&'
+                          'destinations=San+Francisco%%7CVictoria+BC' %
+                          self.key,
+                          responses.calls[0].request.url)
