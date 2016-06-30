c28e73840d82b0914c02347de7113803428fff01
Stephen McDonald <steve@jupo.org>
2016-07-01 09:55:54 +1000

    Tag 2.4.4
    Change-Id: I729b249a4d8bb2c4c59efc7504febc796bb03632

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 ]
 
 setup(name='googlemaps',
-      version='2.4.3-dev',
+      version='2.4.4',
       description='Python client library for Google Maps API Web Services',
       scripts=[],
       url='https://github.com/googlemaps/google-maps-services-python',
