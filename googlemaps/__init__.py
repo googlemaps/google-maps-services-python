ee43a420b085d7baa24227e0d86dddf9f4210d35
Stephen McDonald <steve@jupo.org>
2016-12-01 11:42:11 +1100

    Version 2.4.5-dev
    Change-Id: I56a497c39d6585533aa12ebb0ef4370953747110

--- googlemaps/__init__.py
+++ googlemaps/__init__.py
@@ -15,7 +15,7 @@
 # the License.
 #
 
-__version__ = "2.4.5"
+__version__ = "2.4.5-dev"
 
 from googlemaps.client import Client
 import googlemaps.exceptions
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 ]
 
 setup(name='googlemaps',
-      version='2.4.5',
+      version='2.4.5-dev',
       description='Python client library for Google Maps API Web Services',
       scripts=[],
       url='https://github.com/googlemaps/google-maps-services-python',
