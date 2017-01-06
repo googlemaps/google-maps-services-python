f779f5327fee9f8b0c26a44cd5418c84155fd861
Stephen McDonald <steve@jupo.org>
2017-01-06 15:47:55 +1100

    Fix issue with extra_params + threading. Closes #160.
    Change-Id: Ic9bd0acc8c394bd734bd13b7d10263b2f63915a3

--- googlemaps/client.py
+++ googlemaps/client.py
@@ -328,14 +328,19 @@ def make_api_method(func):
     Provides a single entry point for modifying all API methods.
     For now this is limited to allowing the client object to be modified
     with an `extra_params` keyword arg to each method, that is then used
-    as the params for each web service request. Please note that this is
-    an unsupported feature for advanced use only.
+    as the params for each web service request.
+
+    Please note that this is an unsupported feature for advanced use only.
+    It's also currently incompatibile with multiple threads, see GH #160.
     """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         args[0]._extra_params = kwargs.pop("extra_params", None)
         result = func(*args, **kwargs)
-        del args[0]._extra_params
+        try:
+            del args[0]._extra_params
+        except AttributeError:
+            pass
         return result
     return wrapper
 
