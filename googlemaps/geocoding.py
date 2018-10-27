2b580ca70672c6003125ec926003dafc74f516be
Yevhen Amelin <numrut@users.noreply.github.com>
2018-10-27 12:07:20 +0300

    Fix typos in docstrings

--- googlemaps/geocoding.py
+++ googlemaps/geocoding.py
@@ -43,7 +43,7 @@ def geocode(client, address=None, components=None, bounds=None, region=None,
     :type region: string
 
     :param language: The language in which to return results.
-    :type langauge: string
+    :type language: string
 
     :rtype: list of geocoding results.
     """
@@ -85,7 +85,7 @@ def reverse_geocode(client, latlng, result_type=None, location_type=None,
     :type location_type: list of strings
 
     :param language: The language in which to return results.
-    :type langauge: string
+    :type language: string
 
     :rtype: list of reverse geocoding results.
     """
