2b95ad609146d27baca1bb739386d6ab517ca445
Justin Poehnelt <jpoehnelt@google.com>
2019-08-21 19:00:22 -0700

    more classifier updates

--- setup.py
+++ setup.py
@@ -35,10 +35,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet",
     ],
 )
