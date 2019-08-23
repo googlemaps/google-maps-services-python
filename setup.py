23c73fe902acf6784a4f360f81b7299b1f977ee6
Justin Poehnelt <jpoehnelt@google.com>
2019-08-23 13:48:35 -0700

    set version to 3.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with io.open("README.md", encoding="utf8") as f:
 
 setup(
     name="googlemaps",
-    version="3.0.2",
+    version="3.0.3",
     description="Python client library for Google Maps Platform",
     long_description=readme,
     long_description_content_type="text/markdown",
