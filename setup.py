6b9ffa8cb4946df6ce1e02ff582f6e1345e5ec7f
Justin Poehnelt <jpoehnelt@google.com>
2019-08-23 11:07:39 -0700

    revert version change

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with io.open("README.md", encoding="utf8") as f:
 
 setup(
     name="googlemaps",
-    version="3.0.3rc1",
+    version="3.0.2",
     description="Python client library for Google Maps Platform",
     long_description=readme,
     long_description_content_type="text/markdown",
