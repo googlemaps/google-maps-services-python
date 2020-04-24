5c3fedb2e3993b822db26b2fa5cab3c031d4d21e
Chris Arriola <carriola@google.com>
2020-04-24 09:17:35 -0700

    chore: Install setuptools

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 if ! python3 -m pip --version; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py
-    sudo python3 -m pip install nox
+    sudo python3 -m pip install setuptools nox
 else
-    python3 -m pip install nox
+    python3 -m pip install setuptools nox
 fi
