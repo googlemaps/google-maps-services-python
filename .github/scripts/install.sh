6c902cc38ce7c297cc5cd6d4fe03202b412c78ba
Justin Poehnelt <jpoehnelt@google.com>
2021-03-15 15:58:38 +0000

    chore: run publish in release exec

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -6,7 +6,7 @@ if ! python3 -m pip --version; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py
     sudo python3 -m pip install --upgrade setuptools
-    sudo python3 -m pip install nox
+    sudo python3 -m pip install nox twine
 else
     sudo python3 -m pip install --upgrade setuptools
     python3 -m pip install nox
